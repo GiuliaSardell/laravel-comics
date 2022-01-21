5213bc9638d888c826017dfda88166f902ad6351
GiuliaSardell <giuliasardelli1234@gmail.com>
2022-01-21 19:28:10 +0100

    creazione layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,47 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
   <title>Document</title>
 </head>
 <body>
-  <h1>HOME</h1>
 
   <header>
     <div class="container">
-      <a href="#">
+      <a href="{{ route('home') }}">
         <img src="{{asset('img/dc-logo.png')}}" alt="logo">
       </a>
       <nav>
         <ul>
+
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'characters') ? 'active' : '' }}" href="{{ route('characters') }}">characters</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'movies') ? 'active' : '' }}" href="{{ route('movies') }}">movies</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'tv') ? 'active' : '' }}" href="{{ route('tv') }}">tv</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'games') ? 'active' : '' }}" href="{{ route('games') }}">games</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'collectibles') ? 'active' : '' }}" href="{{ route('collectibles') }}">collectibles</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'videos') ? 'active' : '' }}" href="{{ route('videos') }}">videos</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'fans') ? 'active' : '' }}" href="{{ route('fans') }}">fans</a>
+          </li>
+          <li>
+            <a class="{{ (Route::currentRouteName() === 'news') ? 'active' : '' }}" href="{{ route('news') }}">news</a>
+          </li>
+       
+        
           {{-- <li v-for="(item,index) in links" :key="index">
             <a :href="item.url">{{item.text}}</a>
           </li> --}}
@@ -31,15 +59,27 @@
               </li>
             @endforeach   --}}
 
+          
+
         </ul>
       </nav>
+      
+      <input type="password" class="form-control" id="inputPassword2" placeholder="Search..." style="width: 200px">
+      <button type="submit" class="btn btn-primary">GO</button>
+      
+
     </div>
    
     
   </header>
 
+  <div class="jumbo">
+      
+  </div>
+
   <main> 
     <div class="container">
+      
        
     
       <div class="box">
